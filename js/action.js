422fce85e499f0f72a94dc047d0ea40a036770ad
NDuy248 <lehoangnhatduy2000@gmail.com>
2021-05-07 21:58:29 -0400

    add radix, change resume, change emailSending

--- js/action.js
+++ js/action.js
@@ -185,39 +185,39 @@ function fadeOut(id, time) {
     }, time);
 }
 
-function sendEmail(event) {
-    const emailForm = document.querySelector("#emailForm");
-    var name = emailForm.elements.name;
-    var email = emailForm.elements.email;
-    var mailBody = emailForm.elements.message;
+// function sendEmail(event) {
+//     const emailForm = document.querySelector("#emailForm");
+//     var name = emailForm.elements.name;
+//     var email = emailForm.elements.email;
+//     var mailBody = emailForm.elements.message;
 
-    if (name.value == "" || email.value == "" || mailBody.value == "") {
-        document.getElementById("error").style.display = "inline-block";
-        fadeOut("error", 3000);
-    } else {
-        var mailContent = "Name: " + name.value + ". \nEmail: " + email.value + ". \nMessage: " + mailBody.value;
-        event.preventDefault();
-        Email.send({
-            SecureToken: "25ad52ce-4d77-4b5d-abe9-6d83f5eed1a6",
-            From: "prtflpersonal@gmail.com",
-            To: "lehoangnhatduy2000@gmail.com",
-            Subject: "Message From Portfolio",
-            Body: mailContent
-        }).then(function (message) {
-            if (message == "OK") {
-                document.getElementById("success").style.display = "inline-block";
-                fadeOut("success", 3000);
-                name.value = "";
-                email.value = "";
-                mailBody.value = "";
-            } else {
-                console.log(message)
-                document.getElementById("failure").style.display = "inline-block";
-                fadeOut("failure", 4000);
-            }
-        });
-    }
-}
+//     if (name.value == "" || email.value == "" || mailBody.value == "") {
+//         document.getElementById("error").style.display = "inline-block";
+//         fadeOut("error", 3000);
+//     } else {
+//         var mailContent = "Name: " + name.value + ". \nEmail: " + email.value + ". \nMessage: " + mailBody.value;
+//         event.preventDefault();
+//         Email.send({
+//             SecureToken: "25ad52ce-4d77-4b5d-abe9-6d83f5eed1a6",
+//             From: "prtflpersonal@gmail.com",
+//             To: "lehoangnhatduy2000@gmail.com",
+//             Subject: "Message From Portfolio",
+//             Body: mailContent
+//         }).then(function (message) {
+//             if (message == "OK") {
+//                 document.getElementById("success").style.display = "inline-block";
+//                 fadeOut("success", 3000);
+//                 name.value = "";
+//                 email.value = "";
+//                 mailBody.value = "";
+//             } else {
+//                 console.log(message)
+//                 document.getElementById("failure").style.display = "inline-block";
+//                 fadeOut("failure", 4000);
+//             }
+//         });
+//     }
+// }
 
 function showDescription1() {
     document.getElementById("p1").style.display = "block";
